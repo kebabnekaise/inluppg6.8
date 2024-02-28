b1e754d002e3fdbea7595725a91c905ee3215265
kebabnekaise <emsi06001@utb.vaxjo.se>
2024-02-28 13:07:04 +0100

    Add project files.

---
+++ inluppg6.8/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace inluppg6_8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Skriv in ett tal");
+            int tal = int.Parse(Console.ReadLine());
+
+            int primCounter = 0;
+            for (int i = 1; i < tal; i += 2)
+            {
+                if (isPrimtal(i))
+                {
+                    primCounter++;
+                }
+            }
+            Console.WriteLine($"Det finns {primCounter} primtal mindre än {tal}");
+        }
+
+        static bool isPrimtal(int x)
+        {
+            if (x == 1)
+            {
+                return true;
+            }
+            else if (x == 2)
+            {
+                return true;
+            }
+
+
+            if (x % 2 == 0)
+            {
+                return false;
+            }
+            for (int i = 3; i < x; i += 2)
+            {
+                if (x % i == 0)
+                {
+                    return false;
+                }
+            }
+
+
+            return true;
+        }
+    }
+}
